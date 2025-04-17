a6f1ddee8dba20118ab11251c3e5c48111088009
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-04-17 19:02:18 +0900

    敵にFSMを追加。中身は味方のものと同じ

--- Assets/Scripts/Units/Enemy.cs
+++ Assets/Scripts/Units/Enemy.cs
@@ -3,6 +3,32 @@ using UnityEngine;
 
 public class Enemy : UnitBase
 {
+    public FSM movementFSM;
+    public FSM attackFSM;
+    public void Awake()
+    {
+        movementFSM =
+            new(
+                new()
+                {
+                    { new NearTargetMove(this), new(){ new FSM.Transition(1, () => false) } },
+                }
+            );
+
+        attackFSM =
+            new(
+                new()
+                {
+                    { new NearTargetAttack(this), new(){ new FSM.Transition(3, () => false) } },
+                }
+            );
+    }
+
+    private void Update()
+    {
+        movementFSM.Update();
+        attackFSM.Update();
+    }
 }
 
 public class EnemyMoveLogic : UnitMoveLogicBase
