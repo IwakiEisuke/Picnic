cc9594def7253878a7bc01a64d52d10b875e2d63
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-04-17 18:19:18 +0900

    FSM枠組み

--- Assets/Scripts/Units/UnitMoveLogic.cs
+++ Assets/Scripts/Units/UnitMoveLogic.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -44,4 +47,58 @@ public abstract class UnitMoveLogicBase
     }
 
     public abstract void Start();
+}
+
+public class FSM
+{
+    private Dictionary<IState, List<Transition>> _states = new();
+
+    private IState currentState;
+
+    public FSM(Dictionary<IState, List<Transition>> states, int initStateIndex = 0)
+    {
+        _states = states;
+        currentState = _states.ElementAt(initStateIndex).Key;
+    }
+
+    public void Next(IState newState)
+    {
+        currentState?.Exit();
+        currentState = newState;
+        currentState.Enter();
+    }
+
+    public void Update()
+    {
+        foreach (var transition in _states[currentState])
+        {
+            if (transition.condition.Invoke())
+            {
+                currentState.Exit();
+                currentState = _states.ElementAt(transition.to).Key;
+                currentState.Enter();
+            }
+        }
+
+        currentState.Update();
+    }
+
+    public interface IState
+    {
+        void Enter();
+        void Update();
+        void Exit();
+    }
+
+    public class Transition
+    {
+        public int to;
+        public Func<bool> condition;
+
+        public Transition(int to, Func<bool> condition)
+        {
+            this.to = to;
+            this.condition = condition;
+        }
+    }
 }
