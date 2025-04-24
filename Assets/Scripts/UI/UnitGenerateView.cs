0073a3c4114f1c3ef772d9ca667b37ee253d5032
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-04-24 15:47:48 +0900

    [update] ユニット数表示を追加

--- Assets/Scripts/UI/UnitGenerateView.cs
+++ Assets/Scripts/UI/UnitGenerateView.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,10 +6,12 @@ public class UnitGenerateView : MonoBehaviour
 {
     [SerializeField] UnitGenerateStats stats;
     [SerializeField] Image generateBar;
-
+    [SerializeField] TextMeshProUGUI countText;
+    
     public void View(UnitGenerateStats stats)
     {
         generateBar.fillAmount = stats.ProgressRatio;
+        countText.text = $"{stats.exists}/{stats.MaxCount}";
     }
 
     private void Update()
