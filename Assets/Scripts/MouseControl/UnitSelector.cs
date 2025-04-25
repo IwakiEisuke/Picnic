649f11bcd4490b19652b54566d7e5909223d0326
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-04-25 10:44:29 +0900

    [refactor] UnitSelectorのコード全体をメソッドに分解

--- Assets/Scripts/MouseControl/UnitSelector.cs
+++ Assets/Scripts/MouseControl/UnitSelector.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.UI;
 using UnityEngine.UI;
 
 public class UnitSelector : MonoBehaviour
 {
-    [SerializeField] VirtualMouseInput mouseInput;
     [SerializeField] Image mouseDragArea;
     [SerializeField] GameObject selectMarker;
     [SerializeField] GameObject targetMarker;
@@ -30,36 +28,48 @@ public class UnitSelector : MonoBehaviour
             Destroy(marker);
         }
 
+        UpdateDragState();
+
+        UpdateSelectMarker();
+
+        foreach (var target in targets)
+        {
+            markers.Add(Instantiate(targetMarker, target.position, Quaternion.identity));
+        }
+    }
+
+    void UpdateDragState()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            dragStartMousePos = Input.mousePosition;
+            InitDrag();
         }
 
         if (Input.GetMouseButton(0) && !isDragging && Vector3.Distance(dragStartMousePos, Input.mousePosition) > startDragDistance)
         {
-            isDragging = true;
-            mouseDragArea.enabled = true;
+            StartDrag();
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             if (!isDragging)
             {
-                if (RaycastUnitOnMouse(out var targetHit))
-                {
-                    targets.Add(targetHit.collider.transform);
-                }
-                else
-                {
-                    targets.Clear();
-                }
+                TargetSelectAction();
             }
             else
             {
-                isDragging = false;
+                EndDrag();
             }
         }
 
+        if (isDragging)
+        {
+            Drag();
+        }
+    }
+
+    void UpdateSelectMarker()
+    {
         if (!isDragging && RaycastUnitOnMouse(out var selectHit))
         {
             selectMarker.transform.position = selectHit.transform.position;
@@ -69,71 +79,91 @@ public class UnitSelector : MonoBehaviour
         {
             selectMarker.SetActive(false);
         }
+    }
 
-        if (isDragging)
+    void TargetSelectAction()
+    {
+        if (RaycastUnitOnMouse(out var targetHit))
         {
-            targets.Clear();
-
-            dragEndMousePos = Input.mousePosition;
-
-            var max = new Vector2(Mathf.Max(dragStartMousePos.x, dragEndMousePos.x), Mathf.Max(dragStartMousePos.y, dragEndMousePos.y));
-            var min = new Vector2(Mathf.Min(dragStartMousePos.x, dragEndMousePos.x), Mathf.Min(dragStartMousePos.y, dragEndMousePos.y));
-            mouseDragArea.rectTransform.offsetMax = max;
-            mouseDragArea.rectTransform.offsetMin = min;
-
-            var maxRay = Camera.main.ScreenPointToRay(max);
-            var minRay = Camera.main.ScreenPointToRay(min);
-
-            if (Physics.Raycast(maxRay, out var maxHit) && Physics.Raycast(minRay, out var minHit))
-            {
-                var center = (maxHit.point + minHit.point) / 2;
-                var half = (maxHit.point - minHit.point) / 2;
-
-                Debug.DrawRay(maxHit.point, Vector3.up, Color.cyan);
-                Debug.DrawRay(minHit.point, Vector3.up, Color.cyan);
-
-                Debug.DrawRay(center, Vector3.up, Color.magenta);
-                Debug.DrawLine(center, center + half);
-
-                var cameraRot = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
-                var i_cameraRot = Quaternion.AngleAxis(-Camera.main.transform.eulerAngles.y, Vector3.up);
-
-                half.y = 10f;
-
-                var rotatedHalf = i_cameraRot * half;
-
-                gizmo += () =>
-                {
-                    var rotatedCenter = i_cameraRot * center;
-                    Gizmos.matrix = Matrix4x4.Rotate(cameraRot);
-                    Gizmos.color = new Color(0, 1f, 1f, 0.3f);
-                    Gizmos.DrawCube(rotatedCenter, rotatedHalf * 2);
-                };
-
-                for (int i = 0; i < Physics.OverlapBoxNonAlloc(center, rotatedHalf, cols, cameraRot); i++)
-                {
-                    var col = cols[i];
-
-                    if (CheckUnitInArea(col))
-                    {
-                        Debug.DrawLine(col.transform.position, col.transform.position + Vector3.up * 10, Color.green);
-                        targets.Add(col.transform);
-                    }
-                    else
-                    {
-                        Debug.DrawLine(col.transform.position, col.transform.position + Vector3.up * 10, Color.red);
-                    }
-                }
-            }
+            targets.Add(targetHit.collider.transform);
         }
         else
         {
-            mouseDragArea.enabled = false;
+            targets.Clear();
         }
+    }
 
-        foreach (var target in targets)
+    void InitDrag()
+    {
+        dragStartMousePos = Input.mousePosition;
+    }
+
+    void StartDrag()
+    {
+        isDragging = true;
+        mouseDragArea.enabled = true;
+    }
+
+    void EndDrag()
+    {
+        isDragging = false;
+        mouseDragArea.enabled = false;
+    }
+
+    void Drag()
+    {
+        targets.Clear();
+
+        dragEndMousePos = Input.mousePosition;
+
+        var max = new Vector2(Mathf.Max(dragStartMousePos.x, dragEndMousePos.x), Mathf.Max(dragStartMousePos.y, dragEndMousePos.y));
+        var min = new Vector2(Mathf.Min(dragStartMousePos.x, dragEndMousePos.x), Mathf.Min(dragStartMousePos.y, dragEndMousePos.y));
+        mouseDragArea.rectTransform.offsetMax = max;
+        mouseDragArea.rectTransform.offsetMin = min;
+
+        var maxRay = Camera.main.ScreenPointToRay(max);
+        var minRay = Camera.main.ScreenPointToRay(min);
+
+        if (Physics.Raycast(maxRay, out var maxHit) && Physics.Raycast(minRay, out var minHit))
         {
-            markers.Add(Instantiate(targetMarker, target.position, Quaternion.identity));
+            var center = (maxHit.point + minHit.point) / 2;
+            var half = (maxHit.point - minHit.point) / 2;
+
+            Debug.DrawRay(maxHit.point, Vector3.up, Color.cyan);
+            Debug.DrawRay(minHit.point, Vector3.up, Color.cyan);
+
+            Debug.DrawRay(center, Vector3.up, Color.magenta);
+            Debug.DrawLine(center, center + half);
+
+            var cameraRot = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
+            var i_cameraRot = Quaternion.AngleAxis(-Camera.main.transform.eulerAngles.y, Vector3.up);
+
+            half.y = 10f;
+
+            var rotatedHalf = i_cameraRot * half;
+
+            gizmo += () =>
+            {
+                var rotatedCenter = i_cameraRot * center;
+                Gizmos.matrix = Matrix4x4.Rotate(cameraRot);
+                Gizmos.color = new Color(0, 1f, 1f, 0.3f);
+                Gizmos.DrawCube(rotatedCenter, rotatedHalf * 2);
+            };
+
+            for (int i = 0; i < Physics.OverlapBoxNonAlloc(center, rotatedHalf, cols, cameraRot); i++)
+            {
+                var col = cols[i];
+
+                if (CheckUnitInArea(col))
+                {
+                    Debug.DrawLine(col.transform.position, col.transform.position + Vector3.up * 10, Color.green);
+                    targets.Add(col.transform);
+                }
+                else
+                {
+                    Debug.DrawLine(col.transform.position, col.transform.position + Vector3.up * 10, Color.red);
+                }
+            }
         }
     }
 
