f66d55ce2bef72eef378551fb02bbed2ef1637eb
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-02 18:48:56 +0900

    [update] ユニットに指示を出した際にメニューが閉じるように

--- Assets/Scripts/MouseControl/UnitControlMenu.cs
+++ Assets/Scripts/MouseControl/UnitControlMenu.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitControlMenu : MonoBehaviour
 {
     [SerializeField] UnitSelector unitSelector;
     [SerializeField] RectTransform menuPanel;
+    [SerializeField] Button[] buttons;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         menuPanel.gameObject.SetActive(false);
         unitSelector.OnSelectControlTarget += OpenMenu;
+
+        foreach (var button in buttons)
+        {
+            button.onClick.AddListener(CloseMenu);
+        }
     }
 
     void OpenMenu()
     {
         menuPanel.gameObject.SetActive(true);
         menuPanel.position = Camera.main.WorldToScreenPoint(unitSelector.ControlTarget.position);
+    }
+
+    void CloseMenu()
+    {
+        menuPanel.gameObject.SetActive(false);
     }
 
     private void Update()
