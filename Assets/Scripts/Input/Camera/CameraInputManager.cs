63c136bd8c1e6acdb7eabc4b90fc3e941d4b431a
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-14 19:22:21 +0900

    [update] CameraInputManagerを実装

--- Assets/Scripts/Input/Camera/CameraInputManager.cs
+++ Assets/Scripts/Input/Camera/CameraInputManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// カメラ入力の管理
@@ -7,16 +8,38 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "InputManager/CameraInput")]
 public class CameraInputManager : InputManagerBase
 {
+    [SerializeField] bool debugMode = false;
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float zoomSpeed = 5f;
+
+    [SerializeField] InputActionReference cameraMoveAction;
+    [SerializeField] InputActionReference cameraZoomAction;
+
     public event Action<Vector2> OnCameraMove;
+    public event Action<float> OnCameraZoom;
 
     public override void Init()
     {
-        
+        cameraMoveAction.action.performed += context =>
+        {
+            Vector2 moveInput = context.ReadValue<Vector2>();
+            OnCameraMove?.Invoke(moveSpeed * Time.deltaTime * moveInput);
+            if (debugMode) Debug.Log($"Camera Move Action Performed: {moveInput}");
+        };
+
+        cameraZoomAction.action.performed += context =>
+        {
+            float zoomInput = context.ReadValue<Vector2>().y;
+            OnCameraZoom?.Invoke(zoomSpeed * Time.deltaTime * zoomInput);
+            if (debugMode) Debug.Log($"Camera Zoom Action Performed: {zoomInput}");
+        };
     }
 
     public override void ResetActions()
     {
-        
+        OnCameraMove = null;
+        OnCameraZoom = null;
     }
 
     public override void Update()
