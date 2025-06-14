8488083d3a25ad16c4c264e7101817c184856d2a
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-14 19:00:37 +0900

    [add] CameraInputManagerクラスを追加

---
+++ Assets/Scripts/Input/Camera/CameraInputManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// カメラ入力の管理
+/// </summary>
+[CreateAssetMenu(menuName = "InputManager/CameraInput")]
+public class CameraInputManager : InputManagerBase
+{
+    public event Action<Vector2> OnCameraMove;
+
+    public override void Init()
+    {
+        
+    }
+
+    public override void ResetActions()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        
+    }
+}
