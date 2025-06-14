d34e77362affecf8c9c88b357653b2ee765438ed
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 01:28:41 +0900

    [update] AttackCollisionControllerをAttackControllerに改名。範囲内の一体の敵に直接攻撃をするAttackNearTargetメソッドを追加

---
+++ Assets/Scripts/HitControls/AttackController.cs
@@ -0,0 +1,51 @@
+using System.Linq;
+using UnityEngine;
+
+[DefaultExecutionOrder((int)ExecutionOrder.AttackCollisionController)]
+public class AttackController : MonoBehaviour
+{
+    [SerializeField] Collider[] attackColliders;
+
+    readonly Collider[] overlapColliders = new Collider[10];
+
+    private void Update()
+    {
+        for (int i = 0; i < attackColliders.Length; i++)
+        {
+            attackColliders[i].enabled = false;
+        }
+    }
+
+    public void SetColliderActive(int i)
+    {
+        if (i < 0 || i >= attackColliders.Length)
+        {
+            Debug.LogError($"{name}| SetColliderActive‚Ìˆø”‚Í”ÍˆÍŠO‚Å‚·");
+            return;
+        }
+
+        attackColliders[i].enabled = true;
+    }
+
+    public void AttackNearTarget(Vector3 pos, float radius, AttackData data)
+    {
+        Physics.OverlapSphereNonAlloc(pos, radius, overlapColliders, LayerMask.GetMask("Enemy"), QueryTriggerInteraction.Collide);
+        var target = overlapColliders.Where(x => x != null).OrderBy(x => Vector3.Distance(pos, x.transform.position)).FirstOrDefault();
+
+        if (target != null)
+        {
+            if (target.attachedRigidbody != null && target.attachedRigidbody.TryGetComponent(out HitManager hitManager) || target.TryGetComponent(out hitManager))
+            {
+                var attackInfo = new AttackReceiveInfo
+                {
+                    damage = data.damage
+                };
+                hitManager.ReceiveHit(attackInfo);
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"{name}| AttackNearTarget: No valid target found within range.");
+        }
+    }
+}
