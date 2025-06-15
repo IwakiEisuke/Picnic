4a3d84f4a83fd42137494284526ceaa946110a8c
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 17:12:32 +0900

    [update] ProjectileController.csを追加

---
+++ Assets/Scripts/Entity/Projectile/ProjectileController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 弾オブジェクトを飛ばす／消滅させる
+/// </summary>
+public class ProjectileController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    private void Start()
+    {
+        rb.linearVelocity = transform.forward * 10f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
