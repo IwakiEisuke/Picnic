37fb6edd918e30cb89a2c704916d92dd0e6b079b
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 16:55:03 +0900

    [update] MoveActionの優先度を下げるよう、成功時評価値を0にしました

--- Assets/Scripts/Actions/MoveAction.cs
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -13,10 +13,8 @@ public class MoveAction : ActionBase
         var target = FindObjectsByType<UnitBase>(FindObjectsSortMode.None).Where(x => (_parent.opponentLayer.value & 1u << x.gameObject.layer) > 0).OrderBy(x => Vector3.Distance(x.transform.position, _parent.transform.position)).FirstOrDefault();
         if (target != null)
         {
-            // 距離が遠いほど・足が速いほど評価が高い
             _target = target.transform;
-            float distance = Vector3.Distance(_parent.transform.position, target.transform.position);
-            return (distance - _stats.AttackRadius) * level * _stats.Speed;
+            return 0; // 他のアクションを優先する
         }
 
         return -1;
