f361c8c4f4d68e26b63dce9a0fd0771673ac431c
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 21:17:01 +0900

    [update] DamageAreaAction実装

--- Assets/Scripts/Actions/DamageAreaAction.cs
+++ Assets/Scripts/Actions/DamageAreaAction.cs
@@ -6,13 +6,38 @@
 [CreateAssetMenu(fileName = "DamageAreaAction", menuName = "Actions/DamageAreaAction")]
 public class DamageAreaAction : ActionBase
 {
+    [SerializeField] float baseAttackRange = 5;
+    [SerializeField] float damageAreaRadius = 3;
+    [SerializeField] float duration = 3;
+    [SerializeField] AttackData baseAttackData;
+
+    [SerializeField] GameObject damageAreaPrefab;
+
+    readonly float[] damageMultipliers = { 1, 1.1f, 1.3f};
+    readonly float[] rangeMultipliers = { 1, 1.2f, 1.5f };
+
+    Transform target;
+
+    float Damage => damageMultipliers[level] * (baseAttackData.damage + _stats.Atk);
+    float AttackRange => rangeMultipliers[level] * (baseAttackRange + _stats.AttackRadius);
+
     public override float Evaluate()
     {
-        throw new System.NotImplementedException();
+        if (TryGetNearestAround(transform.position, AttackRange, _parent.opponentLayer, out target))
+        {
+            var targets = CheckAround(target.position, damageAreaRadius, _parent.opponentLayer);
+            return Damage * targets.Length / interval;
+        }
+
+        return -1f;
     }
 
     public override ActionExecuteInfo Execute()
     {
-        throw new System.NotImplementedException();
+        var obj = Instantiate(damageAreaPrefab, target.position, Quaternion.identity);
+        obj.layer = transform.gameObject.layer;
+        obj.GetComponent<AttackCollider>().data = new AttackData(baseAttackData.id, (int)Damage, baseAttackData.invincibleTime);
+        Destroy(obj, duration);
+        return new ActionExecuteInfo(true, this, interval);
     }
 }
