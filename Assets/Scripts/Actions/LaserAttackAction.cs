4e99f9e0ff4dea3c4ecfdb298d8caed9122c1fa0
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 18:47:56 +0900

    [update] LaserAttackActionを実装

--- Assets/Scripts/Actions/LaserAttackAction.cs
+++ Assets/Scripts/Actions/LaserAttackAction.cs
@@ -7,18 +7,42 @@
 public class LaserAttackAction : ActionBase
 {
     [SerializeField, Range(1, 3)] int level = 1;
+
     [SerializeField] float baseAttackRange = 20;
+    [SerializeField] float laserRadius = 3;
+
     [SerializeField] AttackData baseAttackData;
 
-    float Damage => (baseAttackData.damage + _stats.Atk) / interval;
+    float Damage => level * (baseAttackData.damage + _stats.Atk);
+    float AttackRange => level * baseAttackRange + _stats.AttackRadius;
+
+    Transform[] targets;
+    Vector3 laserDirection;
 
     public override float Evaluate()
     {
-        return -1f;
+        targets = CheckAround(transform.position, AttackRange, _parent.opponentLayer);
+
+        if (targets.Length != 0)
+        {
+            laserDirection = (targets[0].position - transform.position).normalized;
+
+            var hitCount = LaserCast(transform.position, laserDirection, AttackRange, laserRadius, _parent.opponentLayer);
+
+            return Damage * hitCount / interval;
+        }
+        else
+        {
+            return -1f;
+        }
     }
 
     public override ActionExecuteInfo Execute()
     {
-        return new ActionExecuteInfo(false);
+        for (int i = 0; i < targets.Length; i++)
+        {
+            _attackController.AttackDirectly(targets[i], new AttackData(baseAttackData.id, (int)Damage, baseAttackData.invincibleTime));
+        }
+        return new ActionExecuteInfo(true, this, interval);
     }
 }
