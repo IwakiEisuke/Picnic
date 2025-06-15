ac44c3a6006123ea249c008d48e289460c84d2f6
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 15:29:46 +0900

    [add] MeleeAreaAttackAction.cs

---
+++ Assets/Scripts/Actions/MeleeAreaAttackAction.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// 近接範囲攻撃
+/// </summary>
+public class MeleeAreaAttackAction : ActionBase
+{
+    public override float Evaluate()
+    {
+        return 0f;
+    }
+
+    public override void Execute()
+    {
+
+    }
+}
