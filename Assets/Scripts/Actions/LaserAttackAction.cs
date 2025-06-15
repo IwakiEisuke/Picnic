625e5713400950e523ac0b4adb5c26cf2dd9b2f4
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 17:35:42 +0900

    [add] LaserAttackActionを追加

---
+++ Assets/Scripts/Actions/LaserAttackAction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 直線状の即時着弾貫通攻撃
+/// </summary>
+[CreateAssetMenu(fileName = "LaserAttackAction", menuName = "Actions/LaserAttackAction")]
+public class LaserAttackAction : ActionBase
+{
+    [SerializeField, Range(1, 3)] int level = 1;
+    [SerializeField] float baseAttackRange = 20;
+    [SerializeField] AttackData baseAttackData;
+
+    float Damage => (baseAttackData.damage + _stats.Atk) / interval;
+
+    public override float Evaluate()
+    {
+        return -1f;
+    }
+
+    public override ActionExecuteInfo Execute()
+    {
+        return new ActionExecuteInfo(false);
+    }
+}
