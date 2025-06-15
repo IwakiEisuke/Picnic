9a02519c8f6e6dc093cdb73ff08ccf8cddf0780e
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 13:06:41 +0900

    [add] 行動を事前に評価するActionBaseクラスを追加

---
+++ Assets/Scripts/Actions/ActionBase.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ActionBase : ScriptableObject
+{
+    protected UnitBase _parent;
+    protected NavMeshAgent _agent;
+    protected UnitStats _stats;
+    protected AttackController _attackController;
+
+    readonly Collider[] _hits = new Collider[10]; // OverlapSphere‚ÌŒ‹‰Ê‚ðŠi”[‚·‚é”z—ñ
+
+    public void Initialize(UnitBase parent)
+    {
+        _parent = parent;
+        _attackController = parent.GetComponent<AttackController>();
+    }
+
+    public virtual float Evaluate() => 0f;
+    public virtual void Execute() { }
+
+    protected Span<Collider> CheckAround(Vector3 position, float radius, LayerMask layerMask)
+    {
+        Physics.OverlapSphereNonAlloc(position, radius, _hits, layerMask.value);
+        Array.Sort(_hits);
+        return _hits.AsSpan();
+    }
+}
