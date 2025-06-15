44727fa48dab231d4b0ad14317bb584642e7bae9
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 16:58:44 +0900

    [update] ProjectileAttackActionを実装

--- Assets/Scripts/Actions/ProjectileAttackAction.cs
+++ Assets/Scripts/Actions/ProjectileAttackAction.cs
@@ -6,13 +6,40 @@
 [CreateAssetMenu(fileName = "ProjectileAttackAction", menuName = "Actions/ProjectileAttackAction")]
 public class ProjectileAttackAction : ActionBase
 {
+    [SerializeField, Range(1, 3)] int level = 1;
+    [SerializeField] float baseAttackRange = 3;
+    [SerializeField] GameObject projectilePref;
+    [SerializeField] AttackData baseAttackData;
+
+    Transform target;
+
+    float Damage => level * (baseAttackData.damage + _stats.Atk);
+
     public override float Evaluate()
     {
-        return 0f;
+        var targets = CheckAround(transform.position, baseAttackRange + _stats.AttackRadius, _parent.opponentLayer);
+
+        if (targets.Length == 0)
+        {
+            return -1;
+        }
+        else
+        {
+            target = targets[0];
+            return Damage / interval;
+        }
     }
 
     public override ActionExecuteInfo Execute()
     {
-        return new ActionExecuteInfo(false);
+        // 弾のプレハブを生成
+        var rot = Quaternion.LookRotation(target.position - transform.position);
+        var obj = Instantiate(projectilePref, transform.position, rot);
+        obj.GetComponent<AttackCollider>().data = new AttackData(baseAttackData.id, (int)Damage, baseAttackData.invincibleTime);
+
+        // その場に留まらせる
+        _agent.SetDestination(transform.position);
+        
+        return new ActionExecuteInfo(true, this, interval);
     }
 }
