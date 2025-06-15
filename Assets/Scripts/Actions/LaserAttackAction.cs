8df293b805646c8b5f371bb7e5915e1bcca26fd9
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 19:21:02 +0900

    [update] レーザーの射出を開始する範囲よりレーザーの長さの方が長くなるよう（「撃っても当たらない」となってほしくないため）laserAdditionalRange変数を追加

--- Assets/Scripts/Actions/LaserAttackAction.cs
+++ Assets/Scripts/Actions/LaserAttackAction.cs
@@ -9,6 +9,7 @@ public class LaserAttackAction : ActionBase
     [SerializeField, Range(1, 3)] int level = 1;
 
     [SerializeField] float baseAttackRange = 20;
+    [SerializeField] float laserAdditionalRange = 5;
     [SerializeField] float laserRadius = 3;
     [SerializeField] float laserDuration = 1;
 
@@ -16,6 +17,7 @@ public class LaserAttackAction : ActionBase
 
     float Damage => level * (baseAttackData.damage + _stats.Atk);
     float AttackRange => level * baseAttackRange + _stats.AttackRadius;
+    float LaserRange => level * (baseAttackRange + laserAdditionalRange) + _stats.AttackRadius;
 
     Transform[] targets;
     Vector3 laserDirection;
@@ -29,7 +31,7 @@ public class LaserAttackAction : ActionBase
         if (around.Length != 0)
         {
             laserDirection = (around[0].position - transform.position).normalized;
-            targets = LaserCast(transform.position, laserDirection, AttackRange, laserRadius, _parent.opponentLayer);
+            targets = LaserCast(transform.position, laserDirection, LaserRange, laserRadius, _parent.opponentLayer);
             return Damage * targets.Length / interval;
         }
         else
@@ -53,7 +55,7 @@ public class LaserAttackAction : ActionBase
         if (laserEmittedTime < laserDuration)
         {
             laserEmittedTime += Time.deltaTime;
-            targets = LaserCast(transform.position, laserDirection, AttackRange, laserRadius, _parent.opponentLayer);
+            targets = LaserCast(transform.position, laserDirection, LaserRange, laserRadius, _parent.opponentLayer);
             _attackController.AttackDirectly(targets, new AttackData(baseAttackData.id, (int)Damage, baseAttackData.invincibleTime));
         }
     }
