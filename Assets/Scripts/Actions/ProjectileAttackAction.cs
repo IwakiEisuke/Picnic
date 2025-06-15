d85c2c57fc7a622110eccfbee1f7c2b2a841291f
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 16:32:44 +0900

    [add] ProjectileAttackAction.csを追加

---
+++ Assets/Scripts/Actions/ProjectileAttackAction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 弾を飛ばす攻撃
+/// </summary>
+[CreateAssetMenu(fileName = "ProjectileAttackAction", menuName = "Actions/ProjectileAttackAction")]
+public class ProjectileAttackAction : ActionBase
+{
+    public override float Evaluate()
+    {
+        return 0f;
+    }
+
+    public override ActionExecuteInfo Execute()
+    {
+        return new ActionExecuteInfo(false);
+    }
+}
