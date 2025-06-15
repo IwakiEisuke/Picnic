a9d8e9743baea8996c8205f8cf52022604ff7afd
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 21:20:05 +0900

    [add] ChargeAttackAction.cs

---
+++ Assets/Scripts/Actions/ChargeAttackAction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 突進攻撃
+/// </summary>
+[CreateAssetMenu(fileName = "ChargeAttackAction", menuName = "Actions/ChargeAttackAction")]
+public class ChargeAttackAction : ActionBase
+{
+    public override float Evaluate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override ActionExecuteInfo Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+}
