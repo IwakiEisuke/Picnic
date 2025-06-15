27509a1db280b53f4dea8d19c47dfe63d9eceb43
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 20:40:45 +0900

    [add] DamageAreaAction.cs

---
+++ Assets/Scripts/Actions/DamageAreaAction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 持続ダメージエリア
+/// </summary>
+[CreateAssetMenu(fileName = "DamageAreaAction", menuName = "Actions/DamageAreaAction")]
+public class DamageAreaAction : ActionBase
+{
+    public override float Evaluate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override ActionExecuteInfo Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+}
