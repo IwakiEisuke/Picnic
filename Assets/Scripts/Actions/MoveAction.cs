2575de21e432a98c528743448fe63830cb9e7b7a
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-15 13:29:54 +0900

    [update] MoveActionの評価値を修正

--- Assets/Scripts/Actions/MoveAction.cs
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -11,18 +11,9 @@ public class MoveAction : ActionBase
         var target = FindObjectsByType<UnitBase>(FindObjectsSortMode.None).Where(x => (x.gameObject.layer & _parent.opponentLayer.value) > 0).OrderBy(x => Vector3.Distance(x.transform.position, _parent.transform.position)).FirstOrDefault();
         if (target != null)
         {
-            // ターゲットが存在する場合、評価値を計算
+            // 距離が遠いほど・足が速いほど評価が高い
             float distance = Vector3.Distance(_parent.transform.position, target.transform.position);
-            if (distance < _stats.AttackRadius)
-            {
-                // ターゲットが攻撃範囲内にいる場合は移動しない
-                return 0f;
-            }
-            else
-            {
-                // ターゲットが攻撃範囲外にいる場合は移動する
-                return (distance - _stats.AttackRadius) * level * _stats.Speed * Time.deltaTime;
-            }
+            return (distance - _stats.AttackRadius) * level * _stats.Speed;
         }
 
         return level * _stats.Speed * Time.deltaTime;
