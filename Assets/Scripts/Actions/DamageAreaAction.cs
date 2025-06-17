205aa49a319063794ea3a8090d3b3f8517ab90c4
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-17 17:04:09 +0900

    [update] DamageAreaを削除してMultipleDamageAreaをDamageAreaに改名

--- Assets/Scripts/Actions/DamageAreaAction.cs
+++ Assets/Scripts/Actions/DamageAreaAction.cs
@@ -9,23 +9,28 @@ public class DamageAreaAction : ActionBase
     [SerializeField] float baseAttackRange = 5;
     [SerializeField] float damageAreaRadius = 3;
     [SerializeField] float duration = 3;
+    [SerializeField] bool positionScattering;
+    [SerializeField] float scatterRadius = 1.5f;
     [SerializeField] AttackData baseAttackData;
+    [SerializeField] bool canMoveWhileExecuting;
+    [SerializeField] Vector3 offset;
 
     [SerializeField] GameObject damageAreaPrefab;
 
-    readonly float[] damageMultipliers = { 1, 1.1f, 1.3f};
+    readonly float[] damageMultipliers = { 1, 1.1f, 1.3f };
     readonly float[] rangeMultipliers = { 1, 1.2f, 1.5f };
 
-    Transform target;
+    Vector3 targetPosition;
 
     float Damage => damageMultipliers[level] * (baseAttackData.damage + _stats.Atk);
     float AttackRange => rangeMultipliers[level] * (baseAttackRange + _stats.AttackRadius);
 
     public override float Evaluate()
     {
-        if (TryGetNearestAround(transform.position, AttackRange, _parent.opponentLayer, out target))
+        if (TryGetNearestAround(transform.position, AttackRange, _parent.opponentLayer, out var target))
         {
             var targets = GetOverlapSphere(target.position, damageAreaRadius, _parent.opponentLayer);
+            targetPosition = target.position;
             return Damage * targets.Length / interval;
         }
 
@@ -34,10 +39,35 @@ public class DamageAreaAction : ActionBase
 
     public override ActionExecuteInfo Execute()
     {
-        var obj = Instantiate(damageAreaPrefab, target.position, Quaternion.identity);
+        if (!canMoveWhileExecuting) _agent.SetDestination(transform.position);
+
+        var obj = Instantiate(damageAreaPrefab, transform.position + offset, Quaternion.identity);
         obj.layer = transform.gameObject.layer;
-        obj.GetComponent<AttackCollider>().data = new AttackData(baseAttackData.id, (int)Damage, baseAttackData.invincibleTime);
+        
+        if (obj.TryGetComponent<ITargetedObject>(out var targetedObject))
+        {
+            var targetPos = targetPosition;
+            if (positionScattering)
+            {
+                targetPos += new Vector3(Random.insideUnitCircle.x, 0, Random.insideUnitCircle.y) * scatterRadius;
+            }
+            targetedObject.InitializeTarget(targetPos, new AttackData(baseAttackData.id, (int)Damage, baseAttackData.invincibleTime));
+        }
+        else
+        {
+            Debug.LogWarning($"{name}: {nameof(DamageAreaAction)}から非{nameof(ITargetedObject)}なインスタンスを生成しました。{nameof(ITargetedObject)}コンポーネントをアタッチしたオブジェクトを入れてください");
+        }
+
+        if (obj.TryGetComponent<IAreaObject>(out var areaObject))
+        {
+            areaObject.InitializeArea(damageAreaRadius);
+        }
+        else
+        {
+            Debug.LogWarning($"{name}: {nameof(DamageAreaAction)}から非{nameof(IAreaObject)}なインスタンスを生成しました。{nameof(IAreaObject)}コンポーネントをアタッチしたオブジェクトを入れてください");
+        }
+
         Destroy(obj, duration);
-        return new ActionExecuteInfo(true, this, interval);
+        return new ActionExecuteInfo(true, this, interval, loopCount, loopInterval);
     }
 }
