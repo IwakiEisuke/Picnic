39cc7f55ce10195559fb50aea40632c1a9578fe2
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-17 16:05:41 +0900

    [add] CurveProjectileController.cs

---
+++ Assets/Scripts/Entity/Projectile/CurveProjectileController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 放射状投射
+/// </summary>
+public class CurveProjectileController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float time = 10f;
+    [SerializeField] float curveHeight = 1f;
+    [SerializeField] float gravity;
+    [SerializeField] GameObject onEndSpawnPrefab;
+    
+    public Vector3 targetPos;
+
+    float t;
+
+    private void Start()
+    {
+        // 高さ考慮してないけどOK
+        var v0 = curveHeight / time;
+        gravity = 2 * v0 * v0 / curveHeight;
+
+        rb.linearVelocity = new Vector3(
+            (targetPos.x - transform.position.x) / time,
+            v0,
+            (targetPos.z - transform.position.z) / time
+        );
+    }
+
+    private void Update()
+    {
+        rb.linearVelocity += -gravity * Time.deltaTime * Vector3.up;
+        t += Time.deltaTime;
+        if (t >= time)
+        {
+            Destroy(gameObject);
+            Instantiate(onEndSpawnPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != gameObject.layer)
+        {
+            Destroy(gameObject);
+            Instantiate(onEndSpawnPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
