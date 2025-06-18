5602bb9eb0ffa76b5c7623eb677d56709d552367
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-18 19:49:17 +0900

    [update] Healthコンポーネントがダメージを受けた際に返すDamageResponseクラスを追加。反射ダメージを格納する

--- Assets/Scripts/Stats/Health.cs
+++ Assets/Scripts/Stats/Health.cs
@@ -1,27 +1,52 @@
-using System;
+﻿using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 [Serializable]
 public class Health : MonoBehaviour
 {
-    [SerializeField] UnitStats stats;
+    [SerializeField] UnitBase unitBase;
     [SerializeField] GameObject dieObj;
 
     public UnityEvent OnDied;
 
     int _currentHealth;
 
-    public float HealthRatio => 1f * _currentHealth / stats.MaxHealth;
+    UnitStats Stats => unitBase.Stats;
+    UnitGameStatus Status => unitBase.status;
+
+    public float HealthRatio => 1f * _currentHealth / Stats.MaxHealth;
 
     public void Start()
     {
-        _currentHealth = stats.MaxHealth;
+        _currentHealth = Stats.MaxHealth;
     }
 
-    public void TakeDamage(AttackReceiveInfo info)
+    public DamageResponse ApplyDamage(AttackReceiveInfo info)
     {
-        _currentHealth -= info.damage;
+        var damage = (int)(info.damage * Mathf.Max(0, (1 - Status.resistance)));
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+
+        // 反射するダメージ量の計算。プレイヤーから見て効果が分かりやすくなるよう、最低１ダメージは与える
+        var reflectDamage = Mathf.CeilToInt(damage * Status.damageReflect);
+
+        if (reflectDamage > 0)
+        {
+            return new DamageResponse(reflectDamage);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void ApplyRawDamage(int value)
+    {
+        _currentHealth -= value;
 
         if (_currentHealth <= 0)
         {
@@ -37,6 +62,18 @@ public class Health : MonoBehaviour
         if (dieObj != null)
         {
             Instantiate(dieObj, transform.position, transform.rotation);
+        }
+    }
+
+    /// <summary>
+    /// 攻撃元に返す反射効果を格納するクラス
+    /// </summary>
+    public class DamageResponse
+    {
+        public int damage;
+        public DamageResponse(int responseDamage)
+        {
+            this.damage = responseDamage;
         }
     }
 }
