2f2d5f379ea99971f88860b433f335b55a461cde
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-19 23:17:54 +0900

    [update] ProjectileAttackActionに集弾率を追加・ループ対応

--- Assets/Scripts/Actions/ProjectileAttackAction.cs
+++ Assets/Scripts/Actions/ProjectileAttackAction.cs
@@ -10,11 +10,12 @@ public class ProjectileAttackAction : ActionBase
     [SerializeField] float bulletRange = 5;
     [SerializeField] bool penetration;
     [SerializeField] float speed = 10f;
+    [SerializeField] float angle = 5;
     [SerializeField] Vector3 projectileOffset;
     [SerializeField] GameObject projectilePref;
     [SerializeField] AttackData baseAttackData;
 
-    Transform target;
+    Vector3 targetPos;
 
     float Damage => level * (baseAttackData.damage + _stats.Atk);
     float AttackTriggerRange => baseAttackRange + _stats.AttackRadius;
@@ -30,7 +31,7 @@ public class ProjectileAttackAction : ActionBase
         }
         else
         {
-            target = targets[0];
+            targetPos = targets[0].position;
             return Damage / interval;
         }
     }
@@ -38,7 +39,7 @@ public class ProjectileAttackAction : ActionBase
     public override ActionExecuteInfo Execute()
     {
         // 弾のプレハブを生成
-        var rot = Quaternion.LookRotation(target.position - transform.position);
+        var rot = Quaternion.LookRotation(targetPos - transform.position) * Quaternion.Euler(0, Random.Range(-angle, angle), 0);
         var obj = Instantiate(projectilePref, transform.position + projectileOffset, rot);
         obj.GetComponent<AttackCollider>().data = new AttackData(baseAttackData.id, (int)Damage, baseAttackData.invincibleTime);
         // 生成元と衝突しないようにレイヤーを設定
@@ -50,6 +51,6 @@ public class ProjectileAttackAction : ActionBase
         // その場に留まらせる
         _agent.SetDestination(transform.position);
         
-        return new ActionExecuteInfo(true, this, interval);
+        return new ActionExecuteInfo(true, this, interval, loopCount, loopInterval);
     }
 }
