d63ef02ca470efc01ed2e017e950a51ab760e236
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-23 18:16:55 +0900

    [add] HasteEffect

---
+++ Assets/Scripts/StatusEffect/HasteEffect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 一定時間の間ユニットの移動速度を上昇させるステータス効果
+/// </summary>
+[CreateAssetMenu(fileName = "HasteEffect", menuName = "Effects/HasteEffect")]
+public class HasteEffect : StatusEffectAssetBase
+{
+    [SerializeField] float effectValue = 1;
+
+    public override void Apply(UnitGameStatus target)
+    {
+        target.speed += effectValue;
+    }
+
+    public override float Evaluate(UnitBase unit)
+    {
+        // 実際にはActionの発動範囲に入るまでが移動時間なので、過大評価されている
+        var remainTime = unit.Agent.remainingDistance / (unit.Status.speed + effectValue);
+        return Mathf.Min(1, remainTime / _duration);
+    }
+}
