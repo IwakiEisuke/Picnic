4c9d0b90d926b41c4bc3425c68f9acab99e3a23a
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-24 19:32:59 +0900

    [add] InteractAction

---
+++ Assets/Scripts/Actions/InteractAction.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// 近いオブジェクトにインタラクトする
+/// </summary>
+[CreateAssetMenu(fileName = "InteractAction", menuName = "Actions/InteractAction")]
+public class InteractAction : ActionBase
+{
+    [SerializeField] EntityType targetType;
+
+    EntityBase target;
+
+    float InteractRadius => _parent.Status.attackRadius;
+
+    public override float Evaluate()
+    {
+        if (_parent.Manager.TryGetNearestEntityAround(_parent, transform.position, InteractRadius, targetType, opponent, selfInclude, out target))
+        {
+            return 1;
+        }
+
+        return -1f;
+    }
+
+    public override ActionExecuteInfo Execute()
+    {
+        if (target is IInteractable interactable)
+        {
+            interactable.Interact();
+        }
+
+        return new ActionExecuteInfo(true, this);
+    }
+}
