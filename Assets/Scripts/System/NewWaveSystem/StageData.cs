eb4bf63662c452c87fcef4864efd3f50c8e849bd
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-06-29 18:43:36 +0900

    [add] 各ウェーブを管理するStageDataクラスを追加

---
+++ Assets/Scripts/System/NewWaveSystem/StageData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "WaveEditor/StageData")]
+public class StageData : ScriptableObject
+{
+    public List<WaveData> waves = new();
+}
