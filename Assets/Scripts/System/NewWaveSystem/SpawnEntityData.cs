54e2e38610718565d948f3697ab124d78dfb8eee
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-07-02 23:26:18 +0900

    [add] スポーンイベント用のエンティティデータを定義するSpawnEntityDataクラスを追加

---
+++ Assets/Scripts/System/NewWaveSystem/SpawnEntityData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// スポーンイベント用のエンティティデータを定義するクラス
+/// </summary>
+[CreateAssetMenu(fileName = "SpawnEntityData", menuName = "Wave System/SpawnEntityData", order = 1)]
+public class SpawnEntityData : ScriptableObject
+{
+    public Texture2D icon;
+    public GameObject entityPrefab;
+}
