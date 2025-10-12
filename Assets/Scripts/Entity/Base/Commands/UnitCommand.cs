40eb6068e5e90444cd6bb974f97f847ba7541f8e
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-10-12 23:15:34 +0900

    [add] ユニット指示クラス追加

---
+++ Assets/Scripts/Entity/Base/Commands/UnitCommand.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ユニットコマンドの定義クラス
+/// </summary>
+[CreateAssetMenu(fileName = "UnitCommand", menuName = "ScriptableObjects/UnitCommand", order = 1)]
+public class UnitCommand : ScriptableObject
+{
+    [SerializeField] string _commandNameKey;
+    [SerializeField] Sprite _icon;
+}
