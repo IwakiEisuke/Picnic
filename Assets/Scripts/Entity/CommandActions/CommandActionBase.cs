cf93f722a1d5c9f64a82c8078cd42153773425f9
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-10-13 23:58:11 +0900

    [add] コマンド指示に応じたユニット行動を定義するクラスを実装

---
+++ Assets/Scripts/Entity/CommandActions/CommandActionBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Entity.CommandActions
+{
+    /// <summary>
+    /// コマンド選択時に実行されるユニット行動の基底クラス
+    /// </summary>
+    public abstract class CommandActionBase : ScriptableObject
+    {
+        public virtual void Init(GameObject gameObject) { }
+        public virtual void Enter() { }
+        public virtual void UpdateAction() { }
+        public virtual void Exit() { }
+    }
+}
