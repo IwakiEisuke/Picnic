0faf2368b336136748c06646ca26180b560091aa
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-10-14 02:07:34 +0900

    [add] ユニット指示の各コマンドを表すコマンドボタンのプレハブと管理クラスの追加

---
+++ Assets/Scripts/UI/CommandButtonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// コマンドメニューのコマンド実行ボタンを管理するクラス
+/// </summary>
+public class CommandButtonController : MonoBehaviour
+{
+    [SerializeField] Image _iconImage;
+    [SerializeField] Button _button;
+
+    public void SetIcon(Sprite icon)
+    {
+        _iconImage.sprite = icon;
+    }
+
+    public void AddButtonAction(UnityEngine.Events.UnityAction action)
+    {
+        _button.onClick.AddListener(action);
+    }
+}
