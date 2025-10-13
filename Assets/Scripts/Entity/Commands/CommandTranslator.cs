c59f949e70877dcff72ef2a750eb487b8456e984
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-10-14 00:01:52 +0900

    [add] コマンド-コマンドアクション間の対応関係を設定するクラスを実装

---
+++ Assets/Scripts/Entity/Commands/CommandTranslator.cs
@@ -0,0 +1,66 @@
+﻿using Entity.CommandActions;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// コマンドとコマンドアクションの橋渡しをするクラス
+/// </summary>
+public class CommandTranslator : MonoBehaviour
+{
+    [SerializeField] TranslateData[] translateDatas;
+
+    [Serializable]
+    class TranslateData
+    {
+        public UnitCommand command;
+        public CommandActionBase commandAction;
+    }
+
+    Dictionary<UnitCommand, CommandActionBase> translateTable = new();
+
+    private void Awake()
+    {
+        foreach (var data in translateDatas)
+        {
+            if (data.command == null)
+            {
+                Debug.LogWarning("CommandTranslator: Command cannot be Null");
+                return;
+            }
+
+            if (data.commandAction == null)
+            {
+                Debug.LogWarning($"CommandTranslator: CommandAction for {data.command} is not assigned");
+                return;
+            }
+            else
+            {
+                data.commandAction = Instantiate(data.commandAction);
+                data.commandAction.Init(gameObject);
+
+                translateTable.Add(data.command, data.commandAction);
+            }
+        }
+    }
+
+    public IEnumerable<UnitCommand> GetCommands()
+    {
+        foreach (var command in translateDatas)
+        {
+            yield return command.command;
+        }
+    }
+
+    /// <summary>
+    /// コマンドからコマンドアクションに変換します。変換先が見つからない場合 null を返します
+    /// </summary>
+    public CommandActionBase Translate(UnitCommand command)
+    {
+        if (translateTable.TryGetValue(command, out var action))
+        {
+            return action;
+        }
+        return null;
+    }
+}
