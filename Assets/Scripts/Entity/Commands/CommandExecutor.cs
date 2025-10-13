bce0305b780538492c0995ec647e891b8a04ae69
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-10-14 01:26:01 +0900

    [add] コマンドに対応するコマンドアクションを実行するクラスを追加

---
+++ Assets/Scripts/Entity/Commands/CommandExecutor.cs
@@ -0,0 +1,49 @@
+﻿using Entity.CommandActions;
+using UnityEngine;
+
+/// <summary>
+/// コマンドに対応するコマンドアクションを実行するクラス
+/// </summary>
+public class CommandExecutor : MonoBehaviour
+{
+    [SerializeField] CommandTranslator _translator;
+    [SerializeField] UnitCommand _defaultCommand;
+
+    CommandActionBase currentCommandAction;
+
+    private void Start()
+    {
+        SetDefault();
+    }
+
+    private void Update()
+    {
+        if (currentCommandAction != null)
+        {
+            currentCommandAction.UpdateAction();
+        }
+    }
+
+    public void Next(UnitCommand command)
+    {
+        var data = _translator.Translate(command);
+        if (data == null)
+        {
+            Debug.LogWarning($"CommandTranslator: No CommandAction found for {command}");
+            return;
+        }
+
+        if (currentCommandAction != null)
+        {
+            currentCommandAction.Exit();
+        }
+
+        currentCommandAction = data;
+        currentCommandAction.Enter();
+    }
+
+    public void SetDefault()
+    {
+        Next(_defaultCommand);
+    }
+}
