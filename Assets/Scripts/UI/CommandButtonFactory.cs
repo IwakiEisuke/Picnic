a0afd6f491c14b6ef4a71cb1b76a86bd0a4d16f1
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-10-14 22:04:54 +0900

    [add] コマンドボタンのファクトリを追加

---
+++ Assets/Scripts/UI/CommandButtonFactory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// コマンドメニューの各コマンド実行ボタンを生成するファクトリ
+/// </summary>
+public class CommandButtonFactory : MonoBehaviour
+{
+    [SerializeField] CommandButtonController _commandButtonPrefab;
+    [SerializeField] Transform _menuParent;
+
+
+    public GameObject CreateCommandCell(UnitCommand command, UnityAction buttonAction)
+    {
+        var controller = Instantiate(_commandButtonPrefab, _menuParent);
+        controller.SetIcon(command.Icon);
+        controller.AddButtonAction(buttonAction);
+
+        var commandButtonObject = controller.gameObject;
+        return commandButtonObject;
+    }
+}
