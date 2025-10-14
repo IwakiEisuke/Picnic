b2b925b84b38ef267805124dba514a8a26fc7563
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-10-14 22:06:26 +0900

    [add] ユニット指示の各コマンドを表示するコマンドメニューの生成・破棄を管理するクラスを実装

---
+++ Assets/Scripts/UI/CommandMenuGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// コマンドメニューの生成・破棄を管理するクラス
+/// </summary>
+public class CommandMenuGenerator : MonoBehaviour
+{
+    [SerializeField] UnitsCommandCollector _commandCollector;
+    [SerializeField] CommandButtonFactory _commandButtonFactory;
+    [SerializeField] UnitController _unitController;
+    [SerializeField] UnitControlMenu _unitControlMenu;
+
+    readonly List<GameObject> _createdCommandButtons = new();
+
+    public void CreateMenu()
+    {
+        _commandCollector.CollectCommands();
+        foreach (var command in _commandCollector.CollectedCommands)
+        {
+            var b = _commandButtonFactory.CreateCommandCell(command, () =>
+            {
+                // ここにコマンド実行処理を追加
+                _unitControlMenu.CloseMenu();
+            });
+
+            _createdCommandButtons.Add(b);
+        }
+    }
+
+    public void ClearMenu()
+    {
+        foreach (var button in _createdCommandButtons)
+        {
+            Destroy(button);
+        }
+        _createdCommandButtons.Clear();
+    }
+}
